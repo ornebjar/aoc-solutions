61cd1aeca9f60950b0923d6356e6ab89596a9e4e
Patrik Örnebjär <patrikbj@gmail.com>
2023-12-09 16:28:32 +0100

    2023: Solve day 9

---
+++ src/main/java/se/pabi/aoc/year2023/Day9.java
@@ -0,0 +1,58 @@
+package se.pabi.aoc.year2023;
+
+import se.pabi.aoc.base.AdventOfCode;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class Day9 extends AdventOfCode<Stream<long[]>> {
+
+    @Override
+    public Stream<long[]> input(String input) {
+        return input.lines().map(line ->
+                Arrays.stream(line.split(" "))
+                        .mapToLong(Long::parseLong)
+                        .toArray()
+        );
+    }
+
+    @Override
+    public Object part1(Stream<long[]> input) {
+        return input.mapToLong(this::calcRight).sum();
+    }
+
+    private long calcRight(long[] line) {
+        int n = line.length - 1;
+        long[] next = new long[n];
+        boolean allZero = true;
+        for (int i = 0; i < n; i++) {
+            if ((next[i] = line[i + 1] - line[i]) != 0) {
+                allZero = false;
+            }
+        }
+        if (allZero) {
+            return line[n];
+        }
+        return line[n] + calcRight(next);
+    }
+
+    @Override
+    public Object part2(Stream<long[]> input) {
+        return input.mapToLong(this::calcLeft).sum();
+    }
+
+    private long calcLeft(long[] line) {
+        long[] next = new long[line.length - 1];
+        boolean allZero = true;
+        for (int i = 0; i < line.length - 1; i++) {
+            if ((next[i] = line[i + 1] - line[i]) != 0) {
+                allZero = false;
+            }
+        }
+        if (allZero) {
+            return line[0];
+        }
+        return line[0] - calcLeft(next);
+    }
+
+}
