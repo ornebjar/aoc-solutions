e0cd6a427642092ce830c9c34c0ca6b351244fe1
Patrik Örnebjär <patrikbj@gmail.com>
2024-12-10 22:54:44 +0100

    2023 Solve day 18 part 1

--- src/main/java/se/pabi/aoc/year2023/Day18.java
+++ src/main/java/se/pabi/aoc/year2023/Day18.java
@@ -3,6 +3,10 @@ package se.pabi.aoc.year2023;
 import se.pabi.aoc.base.AdventOfCode;
 import se.pabi.aoc.util.Util;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
@@ -52,48 +56,100 @@ public class Day18 extends AdventOfCode<Stream<String[]>> {
                 .toArray(Input[]::new));
     }
 
+    private record HLine(long x, long sy, long ey) {
+    }
+
     private static long solve(Input[] inputs) {
-        var min = new Pos(0, 0);
-        var max = new Pos(0, 0);
-        Pos pos = new Pos(0, 0);
+        var linesByStart = new HashMap<Long, ArrayList<HLine>>();
+        var linesByEnd = new HashMap<Long, ArrayList<HLine>>();
+        long min = 0;
+        long max = 0;
+        var pos = new Pos(0, 0);
         for (Input input : inputs) {
-            pos = pos.move(input.dir, input.steps);
-            min = new Pos(Math.min(min.x, pos.x), Math.min(min.y, pos.y));
-            max = new Pos(Math.max(max.x, pos.x), Math.max(max.y, pos.y));
-        }
-
-        int startIndex = 0;
-        for (; startIndex < inputs.length; startIndex++) {
-            if (pos.x == min.x || pos.x == max.x || pos.y == min.y || pos.y == max.y) {
-                break;
+            if ((input.dir & 1) == 1) {
+                HLine line = input.dir == 1
+                        ? new HLine(pos.x, pos.y, pos.y + input.steps)
+                        : new HLine(pos.x, pos.y - input.steps, pos.y);
+                linesByStart.computeIfAbsent(line.sy, k -> new ArrayList<>()).add(line);
+                linesByEnd.computeIfAbsent(line.ey, k -> new ArrayList<>()).add(line);
             }
-            Input input = inputs[startIndex];
+            if (pos.y < min) {
+                min = pos.y;
+            }
+            if (pos.y > max) {
+                max = pos.y;
+            }
             pos = pos.move(input.dir, input.steps);
         }
-
-
-        var prevDir = inputs[startIndex].dir;
-        long prevStep = inputs[startIndex].steps;
-        pos = pos.move(prevDir, prevStep);
-        long exclude = 0L;
-        for (int i = 1; i < inputs.length; i++) {
-            Input input = inputs[i];
-            pos = pos.move(input.dir, input.steps);
-
-            if ((input.dir + 1) % 4 == prevDir) { // left rot
-                exclude += prevStep * input.steps;
-            } else if ((input.dir + 3) % 4 == prevDir) { // right rot
-                prevStep = input.steps;
-                prevDir = input.dir;
-            } else if (input.dir == prevDir) { // same dir
-                prevStep += input.steps;
-            } else { // opposite dir
-                exclude -= prevStep;
-                prevStep = input.steps - prevStep;
-                prevDir = input.dir;
+        var active = new PriorityQueue<>(Comparator.comparingLong(HLine::x));
+        long result = 0;
+        for (long y = min; y <= max; y++) {
+            if (linesByStart.containsKey(y)) {
+                active.addAll(linesByStart.get(y));
+            }
+            boolean include = true;
+            boolean connected = true;
+            var it = active.iterator();
+            HLine from = it.next();
+            HLine to;
+            do {
+                if (from.ey == y) {
+                    it.remove();
+                }
+                to = it.next();
+                if (include) {
+                    if (connected) {
+                        if (from.ey == y && to.sy == y) {
+                            result += to.x - from.x;
+                        } else if (from.ey == y && to.ey == y) {
+                            result += to.x - from.x + 1;
+                            include = false;
+                        } else if (from.sy == y && to.sy == y) {
+                            result += to.x - from.x + 1;
+                            include = false;
+                        } else if (from.sy == y && to.ey == y) {
+                            result += to.x - from.x;
+                        } else {
+                            result += to.x - from.x + 1;
+                            include = false;
+                        }
+                        connected = false;
+                    } else {
+                        result += to.x - from.x + 1;
+                        include = false;
+                        connected = true;
+                    }
+                } else {
+                    if (connected) {
+                        if (from.ey == y && to.sy == y) {
+                            result += to.x - from.x + 1;
+                        } else if (from.ey == y && to.ey == y) {
+                            result += to.x - from.x + 1;
+                            include = true;
+                        } else if (from.sy == y && to.sy == y) {
+                            result += to.x - from.x + 1;
+                            include = true;
+                        } else if (from.sy == y && to.ey == y) {
+                            result += to.x - from.x + 1;
+                        } else {
+                            include = true;
+                        }
+                        connected = false;
+                    } else {
+                        result += to.x - from.x + 1;
+                        include = true;
+                        connected = true;
+                    }
+                }
+                from = to;
+            }
+            while (it.hasNext());
+            if (to.ey == y) {
+                it.remove();
             }
         }
-        return (max.x - min.x + 1) * (max.y - min.y + 1) - exclude;
+
+        return result;
     }
 
 }
