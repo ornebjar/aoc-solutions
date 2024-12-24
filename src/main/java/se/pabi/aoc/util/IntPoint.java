725437131e302b817a03a1df68eb2308a1e97ebb
Patrik Örnebjär <patrikbj@gmail.com>
2024-12-24 11:22:18 +0100

    Solve 2024:21

--- src/main/java/se/pabi/aoc/util/IntPoint.java
+++ src/main/java/se/pabi/aoc/util/IntPoint.java
@@ -1,12 +1,17 @@
 package se.pabi.aoc.util;
 
 public record IntPoint(int x, int y) {
+
     public IntPoint add(int dx, int dy) {
         return new IntPoint(x + dx, y + dy);
     }
 
     public IntPoint add(IntPoint other) {
         return add(other.x, other.y);
+    }
+
+    public IntPoint subtract(IntPoint other) {
+        return new IntPoint(x - other.x, y - other.y);
     }
 
     public IntPoint multiply(int factor) {
@@ -21,6 +26,10 @@ public record IntPoint(int x, int y) {
     public static IntPoint parse(String s) {
         String[] parts = s.split(",");
         return new IntPoint(Integer.parseInt(parts[0]), Integer.parseInt(parts[1]));
+    }
+
+    public int length() {
+        return Math.abs(x) + Math.abs(y);
     }
 
 }
